2213ea4af4bba59aee1a1a04999c22182ab44a22
petkovgogo <petkovgogo@gmail.com>
2020-11-06 17:00:11 +0200

    Added ticket Added route Updated settings.py

--- bdz_project/client/admin.py
+++ bdz_project/client/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ticket, Route
+
+admin.site.register(Ticket)
+admin.site.register(Route)
--- bdz_project/client/models.py
+++ bdz_project/client/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Route(models.Model):
+    starting_station = models.CharField(max_length=100)
+    end_station = models.CharField(max_length=100)
+    
+    def __str__(self):
+        self.starting_station + " -> " + self.end_station
+
+class Ticket(models.Model):
+    price = models.FloatField(default=0.0)
+    departure_date = models.DateTimeField('departure date')
+    train_id = models.IntegerField(default=0)
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.route
