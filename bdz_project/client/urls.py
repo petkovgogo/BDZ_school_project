83af4787502f761f01fba0beaf73fbeb58e2b9c4
Martin Gogov <bloodnighthunt@gmail.com>
2020-11-18 00:47:41 +0200

    revert

--- bdz_project/client/urls.py
+++ bdz_project/client/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'client'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register/', views.register_user, name='register')
+    path('register/', views.register_user, name='register'),
+    path('login/', views.login_user, name='login')
 ]
