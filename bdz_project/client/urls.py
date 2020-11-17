0961e074918f8dcd4d3b4275283f32115b94f315
Martin Gogov <bloodnighthunt@gmail.com>
2020-11-18 01:08:15 +0200

    First register user fix

--- bdz_project/client/urls.py
+++ bdz_project/client/urls.py
@@ -6,6 +6,7 @@ app_name = 'client'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register/', views.register_user, name='register'),
-    path('login/', views.login_user, name='login')
+    path('sign-up/', views.sign_up, name='sign-up'),
+    path('login/', views.login_user, name='login'),
+    path('register/', views.register_user, name='register')
 ]
--- bdz_project/client/views.py
+++ bdz_project/client/views.py
@@ -5,38 +5,26 @@ from django.urls import reverse
 from django.shortcuts import render
 
 def index(request):
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     return render(request, 'client/index.html', {})
-=======
-=======
->>>>>>> f68d3d49e57ff35f7c70ddbec17868ac5d550650
-=======
->>>>>>> 1acd8453871f1046ce56f3fd4d5a7e6dff1bba2d
+    
+def login_user(request):
+    email = request.POST['email']
+    password = request.POST['password']
+    return render(request, 'client/index.html', {})
+
+def sign_up(request):
     return render(request, 'client/register.html', {})
 
-def login_user(request):
-    email = request.POST('email')
-    password = request.POST('password')
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 0b63c8ae82d2415625b2f378e72f7fd0d15c23a9
-=======
->>>>>>> f68d3d49e57ff35f7c70ddbec17868ac5d550650
-=======
->>>>>>> 1acd8453871f1046ce56f3fd4d5a7e6dff1bba2d
-
 def register_user(request):
-    first_name = request.POST('first_name')
-    last_name = request.POST('last_name')
-    username = request.POST('username')
-    email = request.POST('email')
-    password = request.POST('password')
-    conf_password = request.POST('conf_password')
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+    conf_password = request.POST['conf_password']
 
     if password == conf_password:
-        user = User.objects.create(username, email, password)
+        user = User.objects.create_user(username, email, password)
         user.first_name = first_name
         user.last_name = last_name
         user.save()
