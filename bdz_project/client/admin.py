7f0ff223f5c602505a11b9576bac24710745e557
petkovgogo <petkovgogo@gmail.com>
2020-11-17 17:21:29 +0200

    Added basic needed models

--- bdz_project/client/admin.py
+++ bdz_project/client/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-
-from .models import Ticket, Route
+from .models import *
 
 admin.site.register(Ticket)
 admin.site.register(Route)
+admin.site.register(Station)
+admin.site.register(Discount)
--- bdz_project/client/models.py
+++ bdz_project/client/models.py
@@ -1,17 +1,40 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+class Station(models.Model):
+    name = models.CharField(max_length=20)
+    location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 
 class Route(models.Model):
-    starting_station = models.CharField(max_length=100)
-    end_station = models.CharField(max_length=100)
-    
+    starting_station_id = models.ForeignKey(
+        Station, related_name='starting_station_id', default=None, on_delete=models.CASCADE)
+    end_station_id = models.ForeignKey(
+        Station, related_name='end_station_id', default=None, on_delete=models.CASCADE)
+    departure_date = models.DateTimeField('departure date', default=None)
+    arrival_date = models.DateTimeField('arrival date', default=None)
+    duration = models.FloatField(default=0.0)
+    train_id = models.IntegerField(default=0)
+
     def __str__(self):
-        self.starting_station + " -> " + self.end_station
+        return self.starting_station_id.name + " -> " + self.end_station_id.name
+
 
 class Ticket(models.Model):
     price = models.FloatField(default=0.0)
-    departure_date = models.DateTimeField('departure date')
-    train_id = models.IntegerField(default=0)
-    route = models.ForeignKey(Route, on_delete=models.CASCADE)
-    
+    ticket_type = models.CharField(max_length=20, default=None)
+    route = models.ManyToManyField(Route, related_name='route')
+
     def __str__(self):
-        return self.route
+        return "Ticket: " + str(self.pk)
+
+
+class Discount(models.Model):
+    ticket_type = models.CharField(max_length=20)
+    discount = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.ticket_type + ' ticket\' s price = -' + (self.discount * 100) + '%'
