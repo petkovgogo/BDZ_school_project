0ef7a61f0908038076ebd3110c8a01efe30d83ce
petkovgogo <petkovgogo@gmail.com>
2021-01-06 11:20:04 +0200

    Formatted code

--- bdz_project/client/forms.py
+++ bdz_project/client/forms.py
@@ -1,40 +1,80 @@
 import datetime
 from django import forms
-from .models import Station, TicketType
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from .models import Station
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label='Enter your username', max_length=100)
-    password = forms.CharField(label='Enter your password',
-                               max_length=100, min_length=8, widget=forms.PasswordInput)
+    username = forms.CharField(
+        label='Enter your username',
+        max_length=100,
+        widget=forms.TextInput(attrs={'autocomplete': 'chrome-off'})
+    )
+
+    password = forms.CharField(
+        label='Enter your password',
+        max_length=100,
+        min_length=8,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'})
+    )
 
 
 class RegisterForm(forms.Form):
     first_name = forms.CharField(
-        label='Enter your first name', max_length=100, required=False)
+        label='Enter your first name',
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={'autocomplete': 'chrome-off'})
+    )
+
     last_name = forms.CharField(
-        label='Enter your last name', max_length=100, required=False)
-    username = forms.CharField(label='Enter your username', max_length=100)
-    email = forms.EmailField(label='Enter your email', max_length=100)
-    password = forms.CharField(label='Enter your password',
-                               max_length=100, min_length=8, widget=forms.PasswordInput)
-    conf_password = forms.CharField(label='Confirm your password',
-                                    max_length=100, min_length=8, widget=forms.PasswordInput)
+        label='Enter your last name',
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={'autocomplete': 'chrome-off'})
+    )
+
+    username = forms.CharField(
+        label='Enter your username',
+        max_length=100,
+        widget=forms.TextInput(attrs={'autocomplete': 'chrome-off'})
+    )
+
+    email = forms.EmailField(
+        label='Enter your email',
+        max_length=100,
+        widget=forms.TextInput(attrs={'autocomplete': 'chrome-off'})
+    )
+
+    password = forms.CharField(
+        label='Enter your password',
+        max_length=100, min_length=8,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'})
+    )
+
+    conf_password = forms.CharField(
+        label='Confirm your password',
+        max_length=100,
+        min_length=8,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'})
+    )
 
     def clean_username(self):
         username = self.cleaned_data['username'].lower()
-        r = User.objects.filter(username=username)
-        if r.count():
+        username = User.objects.filter(username=username)
+
+        if username.count():
             raise forms.ValidationError("Username already exists")
+
         return username
 
     def clean_email(self):
         email = self.cleaned_data['email'].lower()
-        r = User.objects.filter(email=email)
-        if r.count():
+        password = User.objects.filter(email=email)
+
+        if password.count():
             raise forms.ValidationError("Email already exists")
+
         return email
 
     def clean_password2(self):
@@ -46,41 +86,62 @@ class RegisterForm(forms.Form):
 
         return password2
 
-    def save(self, commit=True):
+    def save(self):
         user = User.objects.create_user(
             self.cleaned_data['username'],
             self.cleaned_data['email'],
             self.cleaned_data['password']
         )
+
         return user
 
 
 class RouteForm(forms.Form):
-    departure_date = forms.DateField(label='Choose a departure date',
-                                     initial=datetime.date.today().strftime('%d.%m.%Y'),
-                                     input_formats=['%d.%m.%Y'],
-                                     widget=forms.DateInput(attrs={'class': 'date-picker'}),
-                                     required=False)
-    arrival_date = forms.DateField(label='Choose an arrival date',
-                                   required=False,
-                                   input_formats=['%d.%m.%Y'],
-                                   widget=forms.DateInput(attrs={'placeholder': 'oneway', 'class': 'date-picker'}),)
+    departure_date = forms.DateField(
+        label='Choose a departure date',
+        initial=datetime.date.today().strftime('%d.%m.%Y'),
+        input_formats=['%d.%m.%Y'],
+        widget=forms.DateInput(attrs={'class': 'date-picker'}),
+        required=False
+    )
+
+    arrival_date = forms.DateField(
+        label='Choose an arrival date',
+        required=False,
+        input_formats=['%d.%m.%Y'],
+        widget=forms.DateInput(
+            attrs={'placeholder': 'oneway', 'class': 'date-picker'})
+    )
+
     departure_station = forms.ModelChoiceField(
-        label='Choose a departure station', queryset=Station.objects.order_by('station_name').all(),
-        required=False)
+        label='Choose a departure station',
+        queryset=Station.objects.order_by('station_name').all(),
+        required=False
+    )
+
     destination = forms.ModelChoiceField(
-        label='Choose a destination', queryset=Station.objects.order_by('station_name').all(),
-        required=False)
-    departure_time = forms.TimeField(label='Departure after', widget=forms.TimeInput(format='%H:%M', attrs={'class': 'time-picker'}),
-    required=False)
+        label='Choose a destination',
+        queryset=Station.objects.order_by('station_name').all(),
+        required=False
+    )
+
+    departure_time = forms.TimeField(
+        label='Departure after',
+        widget=forms.TimeInput(format='%H:%M', attrs={'class': 'time-picker'}),
+        required=False
+    )
 
     def clean_departure_date(self):
         departure_date = self.cleaned_data.get('departure_date')
+
         if departure_date is None:
             raise forms.ValidationError("You must pick a departure date")
+
         curr_date = str(datetime.date.today())
+
         if str(departure_date) < curr_date:
             raise forms.ValidationError("Departure date cannot be in the past")
+
         return departure_date
 
     def clean_arrival_date(self):
@@ -93,22 +154,29 @@ class RouteForm(forms.Form):
         if str(departure_date) <= str(arrival_date):
             raise forms.ValidationError(
                 "Arrival date cannot be before departure date")
+
         return departure_date
-    
+
     def clean_departure_station(self):
         departure_station = self.cleaned_data.get('departure_station')
+
         if departure_station is None:
             raise forms.ValidationError("You must pick a departure station")
+
         return departure_station
-    
+
     def clean_destination(self):
         destination = self.cleaned_data.get('destination')
+
         if destination is None:
             raise forms.ValidationError("You must pick a destination")
+
         return destination
 
     def clean_departure_time(self):
         departure_time = self.cleaned_data.get('departure_time')
+
         if departure_time is None:
             raise forms.ValidationError("You must pick a departure time")
+
         return departure_time
